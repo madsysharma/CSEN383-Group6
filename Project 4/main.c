dad51f7932a88e9351da2d0fb01c4ce40806b582
Yash Suryavanshi <yash.suryavanshi@ymail.com>
2025-02-05 13:47:26 -0800

    autoformat main.c

--- Project 4/main.c
+++ Project 4/main.c
@@ -7,9 +7,9 @@
 #include "helper.h"
 
 // Making a copy of the array of processes
-void copyProcesses(Process *dest, Process* source, int numProcesses)
+void copyProcesses(Process *dest, Process *source, int numProcesses)
 {
-	for(int i = 0; i < numProcesses; i++)
+	for (int i = 0; i < numProcesses; i++)
 	{
 		dest[i].id = source[i].id;
 		dest[i].arrival_time = source[i].arrival_time;
@@ -23,34 +23,34 @@ int main(int argc, char *argv[])
 {
 	srand(time(NULL));
 	int runs = 5;
-	int total_swaps[5] = {0,0,0,0,0};
+	int total_swaps[5] = {0, 0, 0, 0, 0};
 	float hit_ratios[5] = {0.0, 0.0, 0.0, 0.0, 0.0};
 
-	for(int i = 0; i < runs; i++)
+	for (int i = 0; i < runs; i++)
 	{
-		printf("RUN #%d\n", i+1);
+		printf("RUN #%d\n", i + 1);
 		printf("Total processes:%d\n", TOTAL_PROCESSES);
-		Process* processes = (Process*)malloc(TOTAL_PROCESSES * sizeof(Process));
+		Process *processes = (Process *)malloc(TOTAL_PROCESSES * sizeof(Process));
 		generateProcesses(processes, TOTAL_PROCESSES);
-		PageList* plist = (PageList*)malloc(sizeof(PageList));
+		PageList *plist = (PageList *)malloc(sizeof(PageList));
 		initPageList(plist);
 		int run_swaps = 0;
 		float run_ratio = 0.0;
-		 // Display generated processes
+		// Display generated processes
 		printf("\nGenerated Processes:\n");
 		printf("ID\tArrival Time\tService Time\tSize in Pages\n");
-		for (int j = 0; j < TOTAL_PROCESSES; j++) 
+		for (int j = 0; j < TOTAL_PROCESSES; j++)
 		{
 			printf("%d\t%d\t\t%d\t\t%d\n", processes[j].id, processes[j].arrival_time, processes[j].service_time, processes[j].num_pages);
 		}
-        /* Paging algorithm calls and stats calculation done here. Copy original process array into new process array and use that for the paging algorithm.
-        Madhuri - LRU
-        Vaibhav - MFU
-        Yash - LFU
-        Nachiket - FIFO
-        Archana - Random Pick
-        */
-		Process* lruProcesses = (Process*)malloc(TOTAL_PROCESSES * sizeof(Process));
+		/* Paging algorithm calls and stats calculation done here. Copy original process array into new process array and use that for the paging algorithm.
+		Madhuri - LRU
+		Vaibhav - MFU
+		Yash - LFU
+		Nachiket - FIFO
+		Archana - Random Pick
+		*/
+		Process *lruProcesses = (Process *)malloc(TOTAL_PROCESSES * sizeof(Process));
 		copyProcesses(lruProcesses, processes, TOTAL_PROCESSES);
 		lruSimulation(lruProcesses, TOTAL_PROCESSES, plist, &run_swaps, &run_ratio);
 		total_swaps[i] = run_swaps;
@@ -65,11 +65,11 @@ int main(int argc, char *argv[])
 	*/
 	int avg_swaps = 0;
 	float avg_hit_ratio = 0.0;
-	for(int k = 0; k < 5; k++)
+	for (int k = 0; k < 5; k++)
 	{
 		avg_swaps += total_swaps[k];
 		avg_hit_ratio += hit_ratios[k];
 	}
-	printf("Across all 5 runs: average number of successful swaps=%.2f, average hit ratio=%.2f\n", (float)avg_swaps/5, (float)avg_hit_ratio/5);
+	printf("Across all 5 runs: average number of successful swaps=%.2f, average hit ratio=%.2f\n", (float)avg_swaps / 5, (float)avg_hit_ratio / 5);
 	return 0;
 }
